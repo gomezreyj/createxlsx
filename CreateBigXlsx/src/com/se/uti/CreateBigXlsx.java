106b959d28a2ba521ffa7aa7309b70a18c6435c9
gomezreyj <bgmjraral@hotmail.com>
2015-04-17 10:00:19 -0700

    Adding method unpackJar and getPrivateKey

--- CreateBigXlsx/src/com/se/uti/CreateBigXlsx.java
+++ CreateBigXlsx/src/com/se/uti/CreateBigXlsx.java
@@ -1,10 +1,21 @@
 package com.se.uti;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.math.BigDecimal;
+import java.net.JarURLConnection;
+import java.net.URI;
+import java.net.URLConnection;
+import java.security.PrivateKey;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -37,25 +48,62 @@ import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Properties;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Option;
+
+
+
+
+
+
+
+
+
+
+
+
+
+import org.apache.commons.io.FileUtils;
+
+import com.seu.encrypt.*;
 
 public class CreateBigXlsx {
 	
 	public final static String DRIVER = "oracle.jdbc.driver.OracleDriver";
 //	public final static String URL = "jdbc:oracle:thin:@(DESCRIPTION =(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = gisp01-crs.sempra.com)(PORT = 1521))(LOAD_BALANCE = yes))(CONNECT_DATA =(SERVICE_NAME = gisphp03)))";
 	public final static String URL = "jdbc:oracle:thin:";
+	
+	/**
+	   * String to hold the name of the private key file.
+	   */
+	public static final String PRIVATE_KEY_FILE = "irpk";
 
 	static Properties props;
 	
+	/*
+	 This method look to connect to Oracle DB.
+	 Parameters:
+	      service = the description of the service string ( host, port )
+	      user =  username registered in the DB
+	      pass = password of the user
+	 Returns:
+	      An connection object
+	*/
 	public Connection getConnection(String service, String user, String pass)
 	{
-		 //Connect to Oracle
+		//Connect to Oracle
 		//step1 load the driver class   
         try {
 			Class.forName(DRIVER);
@@ -65,18 +113,23 @@ public class CreateBigXlsx {
 		}   
         Connection conn = null;
         try {
-			//conn = DriverManager.getConnection(URL + service,"GFAPDM","fsd.db.03!");
-        	conn = DriverManager.getConnection(URL + service,user,pass);
+			conn = DriverManager.getConnection(URL + service,user,pass);
         	System.out.println("Connected to Oracle");
 		} catch (SQLException e) {
 			System.out.println("ERROR: Getting connection to Oracle");
 			e.printStackTrace();
 		}   
         return conn;
-        //conn = DriverManager.getConnection(URL,props.getProperty("userId").trim(),props.getProperty("password").trim());   
-
 	}
 	
+	/*
+	  This method create a list of queries specified in the XML description file.
+	  Parameters:
+	     doc = The xml document that contains the views used to extract the data
+	  Returns:
+	     A list of queries 
+	 * 
+	 */
 	public List<String> getListQueries(Document doc)
 	{
 		List<String> listQueries = new ArrayList<String>();
@@ -89,14 +142,119 @@ public class CreateBigXlsx {
     	}
 		return listQueries;
 	}
+		
+	/*
+	 This method create a PrivateKey object taking as input the private key file then deserialize the file.
+	 Parameters:
+	     pk = the private key file. This file is referred using an absolute path to the temp umpacked JAR file
+	 Returns:
+	    A PrivateKey object 
+	 */
+	public PrivateKey getPrivateKeyFile(String pk)
+	{
+		 ObjectInputStream inputStream = null;
+		 PrivateKey privateKey = null;
+		 // getting the path to the private key file
+		 
+         try
+         {         
+		     inputStream = new ObjectInputStream(new FileInputStream(pk));
+		     privateKey = (PrivateKey) inputStream.readObject();
+		     inputStream.close();
+         }
+         catch(FileNotFoundException fnf)
+		 {
+			  System.out.println("Private Key file not found.");
+		 } 
+         catch (IOException ioe) 
+         {
+		    ioe.printStackTrace();
+		 } catch (ClassNotFoundException cnfe) {
+			cnfe.printStackTrace();
+		}
+        
+         return privateKey;
+         
+	 }
+	/*
+	 This method unpack the JAR file and create a temporary directory that will hold the private and  password encrypted files.
+	 The purpose of the unpacking is find the temporary absolute path where are located private and password file.
+	 Parameters:
+	    privateKey = name of the private key file
+	    fileNameToDecrypt = name of the password file to be decrypted
+	 Return:
+	   The absolute path location for private key and encrypted password files
+	 */
+	public String unPackJar(String privateKey, String fileNameToDecrypt)
+	{
+		File nf = null;
+		try
+		{
+		    File fj =new File("encviewsreport.jar");
+	        JarFile jf;
+			
+	        jf = new JarFile(fj);
+			Enumeration<JarEntry> entries = jf.entries();
+	        String destdir = "temp1";
+	        
+	          while (entries.hasMoreElements())
+	          {
+	        	  JarEntry entry=(JarEntry)entries.nextElement();
+	        	  if ( entry.getName().toLowerCase().contains(privateKey) || entry.getName().toLowerCase().contains(fileNameToDecrypt))
+	        	  {
+	        		 nf = new File(destdir,entry.getName());
+	        		 
+	        		 if(!nf.exists())
+	                 {
+	                    nf.getParentFile().mkdirs();
+	                    nf = new java.io.File(destdir, entry.getName());
+	                 }
+	        		 java.io.InputStream is;
+					 is = jf.getInputStream(entry);
+					 java.io.FileOutputStream fos;
+					 fos = new java.io.FileOutputStream(nf);
+					
+	        		 while (is.available() > 0) {  // write contents of 'is' to 'fos'
+						    fos.write(is.read());
+					 }
+	        	     fos.close();
+					 is.close();
+	        	 }
+	           }
+	           jf.close();
+	        }
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+	        
+	        String temp1Path = nf.getAbsolutePath().substring(0,nf.getAbsolutePath().lastIndexOf('\\'));
+	        return temp1Path;
+		
+	}
 	
+	/*
+	 * This method delete a directory from the current file system using Apache IO Library
+	 * Parameters:
+	 *   dirName = the name of the directory to delete
+	 */
 	
+	public void deleteDir(String dirName)
+	{
+		try {
+			FileUtils.deleteDirectory(new File(dirName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	
 
     public static void main(String[] args) throws Throwable {
     	
+    	PrivateKey privateK = null;
     	//Get the XML with the parameters to create the report
+    	//File fXmlFile = new File("C:\\ExportReports\\projectqueue_enc.xml");
     	File fXmlFile = new File(args[0]);
         //File fXmlFile = new File("C:\\TestBigXLSX\\CreateBigXlsx\\phmsa.xml");
     	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -107,15 +265,31 @@ public class CreateBigXlsx {
     	
     	CreateBigXlsx bigxlsx = new CreateBigXlsx();
     	String usr = doc.getElementsByTagName("user").item(0).getTextContent();
-    	String pass = doc.getElementsByTagName("pass").item(0).getTextContent();
+    	
+    	// Lets get the file to decrypt from the XML property file
+    	String fileNameToDecrypt = doc.getElementsByTagName("filetodecrypt").item(0).getTextContent();
+    	System.out.println(fileNameToDecrypt);
+    	// Lets get the file to decrypt from the XML property file
+    	String privateKey = doc.getElementsByTagName("pk").item(0).getTextContent();
+    	System.out.println(privateKey);  	
+ 
+    	String pathToPk = bigxlsx.unPackJar(privateKey, fileNameToDecrypt);       
+    	//String pass = doc.getElementsByTagName("pass").item(0).getTextContent();
     	String desc = doc.getElementsByTagName("description").item(0).getTextContent();
     	String outPutFileName = doc.getElementsByTagName("xlsxname").item(0).getTextContent();
-    	 //Getting the query list   	
+    	
+    	//Getting the query list   	
         Iterator<String> iter = bigxlsx.getListQueries(doc).iterator();
-        
-        Connection conn = bigxlsx.getConnection(desc,usr,pass);
+                
+        // Lets get the private key
+        privateK =  bigxlsx.getPrivateKeyFile(pathToPk + "\\" + privateKey);
+        System.out.println("before decrypt");
+        //Lets decrypt the passwd file
+        byte[] dbpass =  EncryptionUtil.decrypt(new File(pathToPk + "\\" + fileNameToDecrypt), privateK);
+        String passwd = new String(dbpass);
+        Connection conn = bigxlsx.getConnection(desc,usr,passwd);
     	PreparedStatement stmt = null;
-    	    	
+    	     	
     	System.out.println("Creating WoorkBook");
         SXSSFWorkbook wb = new SXSSFWorkbook(100); // keep 100 rows in memory, exceeding rows will be flushed to disk
         System.out.println("Finished Creating WoorkBook");
@@ -271,6 +445,7 @@ public class CreateBigXlsx {
         fileOut.close();
         
         System.out.println("Deleting Temporary Files..");
+        bigxlsx.deleteDir("temp1");
         if ( wb.dispose() )
         	System.out.println("Temporary Files Deleted Successfully...");
         else 
