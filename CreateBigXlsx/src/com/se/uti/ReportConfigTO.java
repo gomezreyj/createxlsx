2f74ee1ba3c263c4b82436265f78bf105c7c7b6c
gomezreyj <bgmjraral@hotmail.com>
2015-06-02 15:10:25 -0700

    Adding config TO for report

---
+++ CreateBigXlsx/src/com/se/uti/ReportConfigTO.java
@@ -0,0 +1,58 @@
+package com.se.uti;
+
+public class ReportConfigTO {
+	
+	String user;
+	String filetodecrypt;
+	String service;
+	String xlsxname;
+	String description;
+	String pk;
+	String logfile;
+	
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getFiletodecrypt() {
+		return filetodecrypt;
+	}
+	public void setFiletodecrypt(String filetodecrypt) {
+		this.filetodecrypt = filetodecrypt;
+	}
+	public String getService() {
+		return service;
+	}
+	public void setService(String service) {
+		this.service = service;
+	}
+	public String getXlsxname() {
+		return xlsxname;
+	}
+	public void setXlsxname(String xlsxname) {
+		this.xlsxname = xlsxname;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPk() {
+		return pk;
+	}
+	public void setPk(String pk) {
+		this.pk = pk;
+	}
+	public String getLogfile() {
+		return logfile;
+	}
+	public void setLogfile(String logfile) {
+		this.logfile = logfile;
+	}
+	
+	
+
+}
